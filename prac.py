7ea124d813b6de3e94a7664718881e37f42b4170
Aniruddha <aniruddha@gmail.com>
2024-09-05 05:37:08 +0530

    one program solved

---
+++ prac.py
@@ -0,0 +1,6 @@
+var = "aniruddha 1 45"
+varlist = var.split()
+if varlist[0]=="aniruddha":
+    print(type(varlist[0]))
+    num = int(varlist[1])
+    print(type(num))
--- prog.py
+++ prog.py
@@ -1,1 +1,25 @@
-print("Hello aniruddha")
+if __name__=="main": 
+    N = int(input)
+    list = []
+    for i in range(0,N): 
+        value = input()
+        pak_value = value.split()
+        if pak_value[0]=="insert":
+            e = int(pak_value[1])
+            i = int(pak_value[2])
+            list.insert(e,i)
+        if pak_value[0]=="print":
+            print(list)
+        if pak_value[0]=="append": 
+            e = int[pak_value[1]]
+            list.append(e)
+        if pak_value[0]=="sort": 
+            list.sort()
+        if pak_value[0]=="pop":
+            list.pop()
+        if pak_value[0]=="remove":
+            e = int(pak_value[1])
+            list.remove(e)
+        if pak_value=="reverse":
+            list.reverse()
+            
